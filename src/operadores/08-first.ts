a06d6541a4dc6559a2745e2e7e6539beb97631f0
AntPerDev <antperdev@gmail.com>
2022-07-20 09:20:12 +0200

    operador first

---
+++ src/operadores/08-first.ts
@@ -0,0 +1,32 @@
+/**
+ * first
+ * input$--a--b--c--d--e--f--g--h|----->
+ *       first() /| fist(x => x>=10)
+ *       --a|-------------------------->
+ */
+
+
+
+
+import { first, map, take, tap } from 'rxjs/operators';
+import { fromEvent } from 'rxjs';
+
+
+const click$ = fromEvent<MouseEvent>(document,'click');
+
+click$.pipe( 
+    // take(1),
+    // tap( ()=>console.log('tap')),
+    tap<MouseEvent>( console.log),
+    // map(event=>({
+    //     clientY: event.clientY,
+    //     clientX: event.clientX
+    // }))
+    map( ({clientX, clientY}) =>({ clientY,clientX }) ),
+
+    first( event => event.clientY >= 150 )
+    
+    ).subscribe({
+    next: val => console.log('next:',val),
+    complete: () => console.log('complete')
+});
